4e4e6ece083a52368f7cf2eb5668db3bcc97a2c6
ganesh-48 <ganeshsrananavare@gmail.com>
2021-05-02 08:19:48 +0530

    ADDED: create databse to mysql

--- src/main/java/com/employeepayrollservice/EmployeePayrollService.java
+++ src/main/java/com/employeepayrollservice/EmployeePayrollService.java
@@ -1,7 +1,43 @@
 package com.employeepayrollservice;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+/*@Description- Created a Employee payroll service database.
+ * java program connect to database.*/
 public class EmployeePayrollService {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Payroll service");
+        System.out.println("Welcome to payroll service");
+
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
+        String userName = "root";
+        String password = "root";
+        Connection connection;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded!");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find driver in classpath!", e);
+        }
+        listDrivers();
+
+        try {
+            System.out.println("Connecting to database:" + jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection is successfully!!!" + connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println("  " + driverClass.getClass().getName());
+        }
     }
 }
